2aa32c243cf31df5026196d43af380b4ee9ded3d
Devmal wijesingha <devmalwije1@gmail.com>
2022-12-22 21:10:30 +0530

    initial commit
     created person super class

---
+++ Westminster skin consulation center/src/phase1ToPhase2/Person.java
@@ -0,0 +1,11 @@
+package phase1ToPhase2;
+
+import java.util.Date;
+
+public class Person {
+    private String name;
+    private String surName;
+    private int mobileNumber;
+    private Date dateOfBirth;
+    private String NIC;
+}
