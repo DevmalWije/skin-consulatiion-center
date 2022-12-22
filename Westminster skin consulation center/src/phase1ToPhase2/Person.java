17b0dd53875a74cbe228096ef35301301672fc89
Devmal wijesingha <devmalwije1@gmail.com>
2022-12-22 21:17:15 +0530

    Person class finished
    Person class created with getters and setters

--- Westminster skin consulation center/src/phase1ToPhase2/Person.java
+++ Westminster skin consulation center/src/phase1ToPhase2/Person.java
@@ -1,11 +1,63 @@
 package phase1ToPhase2;
 
 import java.util.Date;
-
+//Creating person super class
 public class Person {
+    //Defining person class arguments
     private String name;
     private String surName;
-    private int mobileNumber;
+    private String mobileNumber;
     private Date dateOfBirth;
     private String NIC;
+    
+    //Creating person class constructor
+    public Person(String name, String surName, String mobileNumber, Date dateOfBirth, String NIC) {
+        this.name = name;
+        this.surName = surName;
+        this.mobileNumber = mobileNumber;
+        this.dateOfBirth = dateOfBirth;
+        this.NIC = NIC;
+    }
+
+    //Creating getters and setters for person class arguments
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getNIC() {
+        return NIC;
+    }
+
+    public void setNIC(String NIC) {
+        this.NIC = NIC;
+    }
+
 }
